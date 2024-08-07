6069bc1c673109ce7b59078c677a12e8ed5d4e59
Ramiro-Elortegui-Bazan <163877562+Ramiro-Elortegui-Bazan@users.noreply.github.com>
2024-08-07 10:29:05 -0300

    Add files via upload

--- RamiroPracticoUno.py
+++ RamiroPracticoUno.py
@@ -1,7 +1,12 @@
 #Escribe un programa que solicite al usuario ingresar 10 números y luego imprima una lista con los números pares ingresados
-numeros=[]
-for i in range(10):
-        numero = int(input(f"Ingresa el número {i + 1}: "))
-        numeros.append(numero)
-NumerosPares=[num for num in numeros if num % 2==0] 
-print (("los numeros pares son "),NumerosPares)
+import time
+import os
+while True:
+        numeros=[]
+        for i in range(10):
+                numero = int(input(f"Ingresa el número {i + 1}: "))
+                numeros.append(numero)
+        NumerosPares=[num for num in numeros if num % 2==0] 
+        print (("los numeros pares son "),NumerosPares)
+        time.sleep(3.0)
+        os.system("clear")
