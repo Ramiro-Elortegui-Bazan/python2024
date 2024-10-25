439388be522efccbcd344f477e2f6660c4d40eb2
Ramiro-Elortegui-Bazan <163877562+Ramiro-Elortegui-Bazan@users.noreply.github.com>
2024-10-25 12:16:25 -0300

    Add files via upload

--- pets.net/script.js
+++ pets.net/script.js
@@ -1,15 +1,20 @@
+// hace que no se ejecute hasta que el DOM este cargado 
 document.addEventListener('DOMContentLoaded', function() {
-    const inputBusqueda = document.getElementById('search-input'); // trae el texto del usuario
-    const botonesVet = document.querySelectorAll('button[data-localidad]'); // selecciona los botones con atributo data-localidad
+//selecciona los elementos de busqueda por su id 
+    const inputBusqueda = document.getElementById('search-input'); 
+    const botonesVet = document.querySelectorAll('button[data-localidad]'); //Selecciona todos los botones que tengan el atributo data-localidad y los almacena en la variable botonesVet. 
 
     inputBusqueda.addEventListener('input', function() {
-        filtroVetLocalidad();
+        filtroVetLocalidad();//usuario escriba algo (evento input), se llama a la función filtroVetLocalidad, para filtarar
+
     });
 
-    function filtroVetLocalidad() {
+    function filtroVetLocalidad() { //lo convierte a minúsculas
         const texto = inputBusqueda.value.toLowerCase();
         botonesVet.forEach(boton => {
-            const localidad = boton.getAttribute('data-localidad').toLowerCase();
+            const localidad = boton.getAttribute('data-localidad').toLowerCase();//minuscula 
+
+            // si aparece en el buscador es visible y si no no xd
             if (localidad.includes(texto)) {
                 boton.style.display = '';
             } else {
